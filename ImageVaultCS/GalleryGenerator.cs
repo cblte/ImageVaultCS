20b59bff31103a6fc30df910a62cbcd3f2e5c534
cblte <cbrueggenolte@me.comn>
2023-04-23 17:12:10 +0200

    implementation of basic gallery generation

---
+++ ImageVaultCS/GalleryGenerator.cs
@@ -0,0 +1,121 @@
+﻿using System.Linq;
+using System;
+using System.IO;
+using System.Text;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace ImageVaultCS;
+
+class GalleryGenerator
+{
+    private readonly string _inputFolder;
+    private readonly string _outputFolder;
+    private readonly int _thumbnailSize;
+    private readonly string _title;
+
+    public GalleryGenerator(string inputFolder, string outputFolder, int thumbnailSize, string title)
+    {
+        _inputFolder = inputFolder;
+        _outputFolder = outputFolder;
+        _thumbnailSize = thumbnailSize;
+        _title = title;
+    }
+
+    public void GenerateGallery()
+    {
+        // Get all image files from the iput folder
+        List<string> imagePaths = Directory.GetFiles(_inputFolder, "*.*", SearchOption.TopDirectoryOnly)
+            .Where(file => file.ToLower().EndsWith(".jpg") ||
+                            file.ToLower().EndsWith(".jpeg") ||
+                            file.ToLower().EndsWith(".png") ||
+                            file.ToLower().EndsWith(".gif"))
+            .ToList();
+        GenerateThumbnailsAndCopy(imagePaths);
+
+        // Generate the HTML for the thumbnail gallery
+        string thumbnailTableHtml = GenerateThumbnailTable(imagePaths);
+
+        // Create the HTML file
+        string htmlFilePath = Path.Combine(_outputFolder, "index.html");
+        using (StreamWriter writer = new StreamWriter(htmlFilePath))
+        {
+            // Write the HTML header
+            writer.WriteLine("<!DOCTYPE html>");
+            writer.WriteLine("<html>");
+            writer.WriteLine("<head>");
+            writer.WriteLine("<link rel=\"stylesheet\" href=\"https://unpkg.com/spectre.css/dist/spectre.min.css\">");
+            writer.WriteLine("<link rel=\"stylesheet\" href=\"https://unpkg.com/spectre.css/dist/spectre-exp.min.css\">");
+            writer.WriteLine("<link rel=\"stylesheet\" href=\"https://unpkg.com/spectre.css/dist/spectre-icons.min.css\">");
+            writer.WriteLine("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            writer.WriteLine($"<title>{_title}</title>");
+            writer.WriteLine("</head>");
+            writer.WriteLine("<body>");
+            writer.WriteLine($"<h1>{_title}</h1>");
+
+            // Write the thumbnail gallery HTML
+            writer.WriteLine(thumbnailTableHtml);
+
+            // Write the HTML footer
+            writer.WriteLine("</body>");
+            writer.WriteLine("</html>");
+        }
+    }
+
+    private void GenerateThumbnailsAndCopy(List<string> imagepPaths)
+    {
+        List<string> thumbnailPaths = new List<string>();
+        foreach (var imagePath in imagepPaths)
+        {
+            string targetPath = Path.Combine(_outputFolder, Path.GetFileName(imagePath));
+            string thumbnailPath = Path.Combine(_outputFolder, Path.GetFileNameWithoutExtension(imagePath) + "_thumb.jpg");
+            // create the image, resize and save it at target location   
+            var image = Image.Load(imagePath);
+            image.Mutate(x => x.Resize(_thumbnailSize, 0));
+            image.SaveAsJpeg(thumbnailPath);
+            image.Dispose();
+            // finally copy the original file
+            File.Copy(imagePath, targetPath, true);
+
+        }
+    }
+
+    private string GenerateThumbnailTable(List<string> imagePaths)
+    {
+        StringBuilder sb = new StringBuilder();
+        // Start the table
+        sb.AppendLine("<table style=\"width:100%;\">");
+        for (int i = 0; i < imagePaths.Count; i += 3)
+        {
+
+            sb.AppendLine("<tr>");
+            // Loop through the next three images
+            for (int j = i; j < i + 3; j++)
+            {
+                // If we have run out of images, add an empty table cell
+                if (j >= imagePaths.Count)
+                {
+                    sb.AppendLine("<td></td>");
+                }
+                else
+                {
+                    string thumbnailPath = Path.GetFileNameWithoutExtension(imagePaths[j]) + "_thumb.jpg";
+                    string targetPath = Path.GetFileName(imagePaths[j]);
+
+                    sb.AppendLine("<td style=\"text-align:center;\">");
+                    // Add Link around the thumbnail
+                    sb.AppendLine($"<a href=\"{targetPath}\" target=\"_blank\">");
+                    // Add an image tag for the thumbnail
+                    sb.AppendLine($"<img src=\"{thumbnailPath}\" alt=\"Thumbnail of {targetPath}\">");
+                    sb.AppendLine("</a>");
+                    sb.AppendLine("</td>");
+                }
+            }
+            // End the row
+            sb.AppendLine("</tr>");
+        }
+        sb.AppendLine("</table>");
+        // Return the generated HTML
+        return sb.ToString();
+    }
+}
--- ImageVaultCS/Program.cs
+++ ImageVaultCS/Program.cs
@@ -1,64 +1,72 @@
-﻿using System;
-using System.IO;
-using System.Runtime.InteropServices;
-using CommandLine;
+﻿using CommandLine;
 using Newtonsoft.Json;
 
-namespace ImageVaultCS
+namespace ImageVaultCS;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Parser.Default.ParseArguments<Options>(args)
-                  .WithParsed<Options>(o =>
+        Parser.Default.ParseArguments<Options>(args)
+              .WithParsed<Options>(o =>
+              {
+                  var inputFolder = o.InputFolder;
+                  var outputFolder = o.OutputFolder;
+                  var configFilePath = o.ConfigFilePath;
+
+                  if (!Directory.Exists(inputFolder))
                   {
-                      var inputFolder = o.InputFolder;
-                      var outputFolder = o.OutputFolder;
-                      var configFilePath = o.ConfigFilePath;
+                      Console.WriteLine($"Input folder '{inputFolder}' does not exist.");
+                      return;
+                  }
 
-                      if (!Directory.Exists(inputFolder))
+                  if (!Directory.Exists(outputFolder))
+                  {
+                      try
                       {
-                          Console.WriteLine($"Input folder '{inputFolder}' does not exist.");
+                          Console.WriteLine($"Output folder '{outputFolder}' does not exist. Trying to creating it.");
+                          // Create the output folder if it doesn't exist
+                          Directory.CreateDirectory(outputFolder);
+                      }
+                      catch (UnauthorizedAccessException ex)
+                      {
+                          // If the user doesn't have write permissions, throw an exception
+                          Console.WriteLine($"Cannot create directory '{outputFolder}' because the user doesn't have write permissions.");
+                          Console.WriteLine($"Exception: {ex}");
                           return;
                       }
+                  }
 
-                      if (!Directory.Exists(outputFolder))
-                      {
-                          Console.WriteLine($"Output folder '{outputFolder}' does not exist.");
-                          return;
-                      }
+                  var config = new Config();
 
-                      var config = new Config();
+                  if (File.Exists(configFilePath))
+                  {
+                      config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configFilePath));
+                  }
 
-                      if (File.Exists(configFilePath))
-                      {
-                          config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configFilePath));
-                      }
-
-                      // TODO: Implement gallery generation using inputFolder, outputFolder, and config
-                  });
-        }
+                  GalleryGenerator gg = new GalleryGenerator(inputFolder, outputFolder, config.ThumbnailSize, config.Title);
+                  gg.GenerateGallery();
+              })
+              .WithNotParsed(errors =>
+              {
+                  // Handle command line parsing errors
+                  foreach (var error in errors)
+                  {
+                      Console.WriteLine(error.ToString());
+                  }
+              });
     }
+}
 
-    class Options
-    {
-        [Option('i', "input", Required = true, HelpText = "The input folder containing images.")]
-        public string InputFolder { get; set; } = "";
 
-        [Option('o', "output", Required = true, HelpText = "The output folder for the generated static site.")]
-        public string OutputFolder { get; set; } = "";
+class Options
+{
+    [Option('i', "input", Required = true, HelpText = "The input folder containing images.")]
+    public string InputFolder { get; set; } = "";
 
-        [Option('c', "config", Required = false, Default = "config.json", HelpText = "The configuration file path.")]
-        public string ConfigFilePath { get; set; } = "";
-    }
+    [Option('o', "output", Required = true, HelpText = "The output folder for the generated static site.")]
+    public string OutputFolder { get; set; } = "";
 
-    class Config
-    {
-        public int ThumbnailSize { get; set; } = 200;
-        public string Title { get; set; } = "My Gallery";
-        public string Description { get; set; } = "A collection of my favorite photos.";
-        public string Theme { get; set; } = "default";
-        // TODO: Add more configurable options as needed
-    }
+    [Option('c', "config", Required = false, Default = "config.json", HelpText = "The configuration file path.")]
+    public string ConfigFilePath { get; set; } = "";
 }
